4bb87c660203fdaa04d9a51bc0bda4d0e95b8309
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2023-03-09 13:39:06 +0000

    artist

--- first-routed-app/src/environments/environment.ts
+++ first-routed-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    oauthToken: "Bearer BQCBZybrwsFToy5y1CqP0ZGFhitvKLHucHyUVXMlM3ZwJO4rZINKzMZSlq4fyxZgL2RDEPQDqM1nsI1Za0y5HqsjTgAmuDRA40OLbAzzxV9FP3Uqyz2a5B2R0tRDZ-cid6sOA1h6OrhG_Lqogk54NlgHsUVu2c7xUhi5n2fxd6GWp5Z7S4HHUHMcaGwLvTPbIxjH"
+    oauthToken: "Bearer BQDw1YxguJYk4djI4z5q2hwW0PuOEsru4rR7eV6O3x1hZ2uQhxAhQzopbC8oCgMR2QwzXM4b26rr4rKGRiLTVDQiVXqqVeOc_44z7m_Dut-xEw6ZNKRrayVa9vOYkpnoncXHa9EnMTWCMnSV5zZStH6m-3Vlq_ZJv0AfEAkC4vxAPPh2I8u16S5jDjo9k8vnUc-n"
   };
   
   
