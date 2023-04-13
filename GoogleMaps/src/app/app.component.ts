904d00300d3c8c80992cfef17bc84e4101e5c8c9
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2023-04-13 12:57:46 +0000

    maps

--- GoogleMaps/src/app/app.component.ts
+++ GoogleMaps/src/app/app.component.ts
@@ -11,11 +11,28 @@ export class AppComponent {
   center : any;
   position : any;
   label :string;
+  circleOptions: { fillColor: string; };
+  markerOptions: { icon: google.maps.Icon; };
+  vertices: { lat: any; lng: any; }[];
+  
   constructor()
   {
+    let iconData : google.maps.Icon = {
+      url:'./assets/img/cat_acrobat.ico',
+      scaledSize : new google.maps.Size(60,60)
+    }
+    this.markerOptions = {icon:iconData}
+
+    this.circleOptions = {fillColor : 'red'};
     this.center={lat: 45.506738, lng: 9.190766};
     this.position = this.center;
-    this.label = "ciao";
+    this.label = "Qua";
+    this.vertices = [
+      {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      {  lat: this.center.lat, lng: this.center.lng },
+      {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002}
+    ];   
+    
   }
 
 }
