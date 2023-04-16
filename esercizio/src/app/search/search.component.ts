2f06ee3e0adfd515ec989c76051ec2a9636a1fec
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2023-04-16 19:22:45 +0000

    ricerca

--- esercizio/src/app/search/search.component.ts
+++ esercizio/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PLayersService } from '../Gioca.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  query!: string;
+  results: any;
+  obsBeer!: Observable<Object>;
+  constructor(public beerService: PLayersService) {}
+
+  submit(query:HTMLInputElement): void {
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsBeer = this.beerService.searchBeer(this.query);
+    this.obsBeer.subscribe((data) => this.results = data); 
+  }
 
 }
