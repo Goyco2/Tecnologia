367007a1c8fb8611a4313cbc1517f1986958c37d
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2023-04-16 19:10:48 +0000

    componeti

---
+++ esercizio/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CaratteristicheComponent } from './caratteristiche/caratteristiche.component';
+import { SearchComponent } from './search/search.component';
+
+const routes: Routes = [
+  {path: 'search', component: SearchComponent},
+  {path: 'caratteristiche/:id', component: CaratteristicheComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
