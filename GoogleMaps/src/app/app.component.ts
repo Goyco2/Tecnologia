8ce692a06857a63df21463797bd846d3475af968
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2023-05-04 12:59:58 +0000

    02.2

--- GoogleMaps/src/app/app.component.ts
+++ GoogleMaps/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,38 +9,56 @@ export class AppComponent {
   //Aggiungiamo latitudine e longitudine di un luogo
   center : any;
   position : any;
+  position2 : any;
+  position3: any;
   label :string;
   circleOptions: any;
-  markerOptions: { icon: google.maps.Icon; };
-  vertices: { lat: any; lng: any; }[];
-  
-  constructor()
-  {
+  markerOptions: google.maps.MarkerOptions;
+  markerOptions2: google.maps.MarkerOptions;
+  markerOptions3: google.maps.MarkerOptions;
+  vertices: google.maps.LatLngLiteral[];
+
+  constructor() {
+    this.center={lat: 45.506738, lng: 9.190766};
+    this.position = this.center;
+    this.position2 = {lat: 45.506958, lng: 9.191766};
+    this.position3 = {lat: 45.506358, lng: 9.191766};
+    this.label = "ciao";
+    this.circleOptions = {fillColor : 'red'}
+
     let iconData : google.maps.Icon = {
       url:'./assets/img/cat_acrobat.ico',
       scaledSize : new google.maps.Size(60,60)
     }
-    this.markerOptions = {icon:iconData};
-    this.circleOptions = {fillColor : 'red'};
-    this.center={lat: 45.506738, lng: 9.190766};
-    this.position = this.center;
-    this.label = "Qua";
+    let iconData2 : google.maps.Icon = {
+      url:'./assets/img/cane.png',
+      scaledSize : new google.maps.Size(100,100)
+    }
+    let iconData3 : google.maps.Icon = {
+      url:'./assets/img/ping.png',
+      scaledSize : new google.maps.Size(60,60)
+    }
+
+    this.markerOptions = {icon:iconData}
+    this.markerOptions2 = {icon:iconData2}
+    this.markerOptions3 = {icon:iconData3}
+
     this.vertices = [
       {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
       {  lat: this.center.lat, lng: this.center.lng },
       {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002}
-    ];   
-  }
-    cambiaColreB() {
-      this.circleOptions = {fillColor : 'blue'}
-    }
-    cambiaColreG() {
-      this.circleOptions = {fillColor : 'green'}
-    }
-    cambiaColreR() {
-      this.circleOptions = {fillColor : 'red'}
-    }
-
+ ];
   }
 
+  cambiaColoreRosso() {
+    this.circleOptions = {fillColor : 'red'}
+  }
+  cambiaColoreBlu() {
+    this.circleOptions = {fillColor : 'blue'}
+  }
+  cambiaColoreGiallo() {
+    this.circleOptions = {fillColor : 'yellow'}
+  }
 
+
+}
